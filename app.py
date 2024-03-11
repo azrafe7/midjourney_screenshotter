443f001bcb20557c12c1f2a5e03f87fbfe9561b9
Giuseppe Di Mauro <azrafe7@gmail.com>
2024-03-11 02:28:19 +0100

    trying to make it work with selenium uc driver

--- app.py
+++ app.py
@@ -1,58 +1,75 @@
-from playwright.sync_api import sync_playwright
+# from playwright.sync_api import sync_playwright
+from selenium import webdriver
+import undetected_chromedriver as uc
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 import settings
 import re
 
 
 if __name__ == "__main__":
-  with sync_playwright() as p:
     print("Launching " + ("Headless " if settings.headless_browser else "") + "Browser...")
 
-    browser = p.chromium.launch(headless=settings.headless_browser)
-    context = browser.new_context(user_agent=settings.user_agent)
-    context.set_default_timeout(settings.default_timeout)
+    options = webdriver.ChromeOptions()
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-extensions")
+    options.headless = settings.headless_browser
+    options.capabilities["timeouts"] = {
+      "implicit": settings.default_timeout,
+      "pageLoad": settings.default_timeout,
+      "script": settings.default_timeout
+    }
 
-    page = browser.new_page()
+    # driver = webdriver.Chrome(options=options)
+    driver = uc.Chrome(options=options)
 
     print(f"Going to '{settings.MIDJOURNEY_URL}'...")
 
-    # page.goto(settings.MIDJOURNEY_URL, referer="https://google.com")
-    page.goto("https://www.google.com/search?q=midjourney+feed")
+    # breakpoint()
 
-    breakpoint()
+    driver.get(settings.MIDJOURNEY_URL)
+    # driver.get("https://www.google.com/search?q=midjourney+feed")
+    navigator_webdriver = driver.execute_script('return navigator.webdriver')
+    print(f"navigator.webdriver: {navigator_webdriver}")
 
-    midjourney_anchor = page.locator('a[href^="https://www.midjourney.com/showcase"]').first
-    if not midjourney_anchor.is_visible():
-      breakpoint()
+    # breakpoint()
 
-    midjourney_anchor.dispatch_event('click')
+    # curr_url = page.url
+    # not_curr_url_regex = re.compile('^(?!' + curr_url + ')')
 
-    breakpoint()
+    # # page.goto(midjourney_anchor.get_attribute("href"))
 
-    curr_url = page.url
-    not_curr_url_regex = re.compile('^(?!' + curr_url + ')')
+    # # first_result_loc = page.locator(
 
-    # page.goto(midjourney_anchor.get_attribute("href"))
+    # page.wait_for_load_state("load")
 
-    # first_result_loc = page.locator(
+    # page_scroll_selector = '#pageScroll'
+    # page.wait_for_selector(page_scroll_selector, state='visible')
+    # page_scroll_loc = page.locator(page_scroll_selector).first
 
-    page.wait_for_load_state("load")
-
-    page_scroll_selector = '#pageScroll'
-    page.wait_for_selector(page_scroll_selector, state='visible')
-    page_scroll_loc = page.locator(page_scroll_selector).first
-
-    screenshot_filename = "page.png"
-    page.screenshot(path=screenshot_filename)
-    print(f"Saved screenshot to '{screenshot_filename}'...")
+    # screenshot_filename = "page.png"
+    # page.screenshot(path=screenshot_filename)
+    # print(f"Saved screenshot to '{screenshot_filename}'...")
 
     with open("get_bg_cover_links.js") as f:
       js_script = f.read()
 
-    breakpoint()
+    # breakpoint()
 
-    res = page.evaluate(js_script)
+    driver.set_script_timeout(30)
+    res = driver.execute_script(js_script)
 
-    breakpoint()
+    # breakpoint()
 
+    wait = WebDriverWait(driver, 5)
+    element_locator = (By.CSS_SELECTOR, '#__hidden_bg_text_area')
+    element = wait.until(EC.presence_of_element_located(element_locator))
 
-    # browser.close()
+    # now `res` is actionable... (or can be grabbed via textarea)
+    print("Scraped:")
+    print(res)
+
+    print()
